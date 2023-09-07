eca98d43f7e71b55980d98f53979ecf38afa5b20
ManuelAchaval <manuachaval@hotmail.com>
2023-09-07 19:44:22 -0300

    styles

--- .web/pages/index.js
+++ .web/pages/index.js
@@ -1,9 +1,9 @@
 import { Fragment, useContext, useEffect, useRef, useState } from "react"
 import { useRouter } from "next/router"
-import { E, getAllLocalStorageItems, getRefValue, getRefValues, isTrue, preventDefault, refs, uploadFiles, useEventLoop } from "/utils/state"
+import { E, getAllLocalStorageItems, getRefValue, getRefValues, isTrue, preventDefault, refs, set_val, uploadFiles, useEventLoop } from "/utils/state"
 import { EventLoopContext, StateContext } from "/utils/context.js"
 import "focus-visible/dist/focus-visible"
-import { Box, Button, Heading, HStack, Text, useColorMode, VStack } from "@chakra-ui/react"
+import { Box, Button, Container, HStack, Input, Text, useColorMode } from "@chakra-ui/react"
 import NextHead from "next/head"
 
 
@@ -35,17 +35,40 @@ export default function Component() {
 
   return (
   <Fragment><Fragment>
-  <VStack>
+  <Container>
+  <Box>
+  <Box sx={{"marginY": "1em"}}>
+  <Box sx={{"textAlign": "right"}}>
+  <Text sx={{"padding": "1em", "borderRadius": "5px", "marginY": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "bg": "#F5EFFE", "marginLeft": "20%"}}>
+  {`What is Reflex?`}
+</Text>
+</Box>
+  <Box sx={{"textAlign": "left"}}>
+  <Text sx={{"padding": "1em", "borderRadius": "5px", "marginY": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "bg": "#DEEAFD", "marginRight": "20%"}}>
+  {`A way to build web apps in pure Python!`}
+</Text>
+</Box>
+</Box>
+  <Box sx={{"marginY": "1em"}}>
+  <Box sx={{"textAlign": "right"}}>
+  <Text sx={{"padding": "1em", "borderRadius": "5px", "marginY": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "bg": "#F5EFFE", "marginLeft": "20%"}}>
+  {`What can I make with it?`}
+</Text>
+</Box>
+  <Box sx={{"textAlign": "left"}}>
+  <Text sx={{"padding": "1em", "borderRadius": "5px", "marginY": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "bg": "#DEEAFD", "marginRight": "20%"}}>
+  {`Anything from a simple website to a complex web app!`}
+</Text>
+</Box>
+</Box>
+</Box>
   <HStack>
-  <Button colorScheme={`red`} sx={{"borderRadius": "1em"}}>
-  {`Decrement`}
-</Button>
-  <Heading sx={{"fontSize": "2em"}}/>
-  <Button colorScheme={`green`} sx={{"borderRadius": "1em"}}>
-  {`increment`}
+  <Input placeholder={`Ask a question`} sx={{"borderWidth": "1px", "padding": "1em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px"}} type={`text`}/>
+  <Button sx={{"bg": "#CEFFEE", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px"}}>
+  {`Ask`}
 </Button>
 </HStack>
-</VStack>
+</Container>
   <NextHead>
   <title>
   {`Reflex App`}
--- hello_reflex/hello_reflex.py
+++ hello_reflex/hello_reflex.py
@@ -1,23 +1,51 @@
 import reflex as rx
+from hello_reflex import style
 
-def index():
-    return rx.vstack(
-        rx.hstack(
-        rx.button(
-            "Decrement",
-            color_scheme="red",
-            border_radius="1em"
+
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right",
         ),
-        rx.heading(font_size="2em"),
-        rx.button(
-            "increment",
-            color_scheme="green",
-            border_radius="1em"
-        )
+        rx.box(
+            rx.text(answer, style=style.answer_style), 
+            text_align="left",
+        ),
+        margin_y="1em",
     )
 
+
+def chat() -> rx.Component:
+    qa_pairs = [
+        (
+            "What is Reflex?",
+            "A way to build web apps in pure Python!",
+        ),
+        (
+            "What can I make with it?",
+            "Anything from a simple website to a complex web app!",
+        ),
+    ]
+    return rx.box(
+        *[
+            qa(question, answer)
+            for question, answer in qa_pairs
+        ]
     )
 
-app=rx.App()
+def action_bar() -> rx.Component:
+    return rx.hstack(
+        rx.input(placeholder="Ask a question", style=style.input_style),
+        rx.button("Ask", style=style.button_style),
+    )
+
+
+def index() -> rx.Component:
+    return rx.container(
+        chat(),
+        action_bar()
+    )
+app = rx.App()
 app.add_page(index)
 app.compile()
